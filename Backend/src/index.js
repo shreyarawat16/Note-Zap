1a2fa9491562239e905e00e412c18defb577c416
Shreya <shreyarawat353@gmail.com>
2025-08-08 18:17:34 +0530

    new commits

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,20 +5,34 @@ import {connectDB} from './config/data.js'
 import dotenv from "dotenv";
 import rateLimiter from './middleware/rateLimiter.js'
 import cors from "cors";
+import path from "path"
 
 dotenv.config();
 const PORT= process.env.PORT || 8080;
+const __dirname = path.resolve();
 
-app.use(cors({
-   origin: "http://localhost:5173"
-})); //this is a middleware which allow every request from every other URL
-
+//this is only needed in development, in production the frontend will be served from the same server
+if(process.env.NODE_ENV !== "production"){ 
+//this is a middleware which allow every request from every other URL
+if(process.env.CORS === "true"){
+   app.use(cors({
+      origin: "http://localhost:5173"
+   }));
+   }
+}
 app.use(express.json()); //this middleware parse JSON data in req.body
 app.use(rateLimiter);
 
 
 app.use("/api/notes", notesRoutes );
 
+if(process.env.NODE_ENV === "production"){
+app.use(express.static(path.join(__dirname,"../frontend/noteapp/dist")));
+
+app.get("*", (req,res)=>{
+   res.sendFile(path.join(__dirname, "../frontend/noteapp/dist/index.html"));
+})
+}
 //first connect to the database, then start to listen on the port
 connectDB().then(()=>{
  app.listen(PORT, ()=>{
