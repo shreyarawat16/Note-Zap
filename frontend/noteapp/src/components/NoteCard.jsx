c58ae37a176ff658e6baf124882c37dff47a6bdb
Shreya <shreyarawat353@gmail.com>
2025-08-11 17:09:06 +0530

    notes

--- frontend/noteapp/src/components/NoteCard.jsx
+++ frontend/noteapp/src/components/NoteCard.jsx
@@ -23,8 +23,8 @@ export default function NoteCard({note, setNotes}) {
   }
   return (
     <>
-    <Link to={`/note/${note._id}`} className=" p-4 md:p-6 card bg-base-100 border-t-4 sm:h-40 sm:w-70 md:h-60 md:w-90 hover:shadow-xl transition-all duration-200 border-solid border-[#00FF9D]" >
-      <div className="card-body bg-base-500 shadow-2xl w-90">
+    <Link to={`/note/${note._id}`} className=" p-4 md:p-6 card bg-base-100 border-t-4 sm:h-50 sm:w-70 md:h-60 md:w-90 hover:shadow-xl transition-all duration-200 border-solid border-[#00FF9D]" >
+      <div className="card-body bg-base-500 shadow-2xl sm:w-70 md:w-90">
         <h2 className="card-title text-md font-bold ">{note.title} </h2>
         <p className='text-base-content/80 text-[15px] md:text-[16px] flex-start'>{note.content} </p>
         <div className="card-actions flex justify-between items-center">
